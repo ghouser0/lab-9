6e4ea4a007714c1cd13cc7afaaa2fc33cdb1fdc6
allie <allisontivvis@ufl.edu>
2024-04-11 11:20:40 -0400

    added new changes

--- lab9github.py
+++ lab9github.py
@@ -14,22 +14,35 @@ def encode(password):
         encoded_pass += encoded_bit
     return encoded_pass
 
+def decode(password):
+    decoded_pass = ""
+    for bit in password:
+        decoded_bit = str(int(bit) - 3)
+        decoded_pass += decoded_bit
+    return decoded_pass
+
 
 def main():
-    menu()
-    option = int(input("Please enter an option: "))
-    if option == 1:
-        password = input("Please enter your password to encode: ")
-        encoded_pass = encode(password)
-        print("Your password has been encoded and stored!")
+    original_pass = ""
 
-    elif option == 2:
-        pass
+    while True:
+        menu()
+        option = int(input("Please enter an option: "))
+        if option == 1:
+            password = input("Please enter your password to encode: ")
+            encoded_pass = encode(password)
+            original_pass = password
+            print("Your password has been encoded and stored!")
 
-    elif option == 3:
-        quit
+        elif option == 2:
+            if original_pass:
+                decoded_pass = decode(encoded_pass)
+                print(f"The encoded password is {encoded_pass}, and the original password is {original_pass}.")
+            else:
+                print("Please encode a password first.")
 
-
+        elif option == 3:
+            break
 
 if __name__ == "__main__":
     main()
